b9e3f99d743ee3bb95a09e5d27528ca9cc31c35f
Tom Riley <tom@smallroomsoftware.com>
2012-04-07 14:07:28 +0100

    Added roo dependency

--- flatfile_importer.gemspec
+++ flatfile_importer.gemspec
@@ -8,9 +8,9 @@ Gem::Specification.new do |s|
   s.version = "0.1.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
-  s.authors = ["tomriley"]
-  s.date = "2012-04-06"
-  s.description = "TODO: longer description of your gem"
+  s.authors = ["Tom Riley"]
+  s.date = "2012-04-07"
+  s.description = "framework for writing spreadsheet file importers"
   s.email = "tom@smallroomsoftware.com"
   s.extra_rdoc_files = [
     "LICENSE.txt",
@@ -19,33 +19,41 @@ Gem::Specification.new do |s|
   s.files = [
     ".document",
     "Gemfile",
+    "Gemfile.lock",
     "LICENSE.txt",
     "README.rdoc",
     "Rakefile",
     "VERSION",
-    "test/helper.rb"
+    "flatfile_importer.gemspec",
+    "lib/flatfile_importer.rb",
+    "lib/flatfile_importer/importer.rb",
+    "test/helper.rb",
+    "test/test_flatfile_importer.rb"
   ]
   s.homepage = "http://github.com/tomriley/flatfile_importer"
   s.licenses = ["MIT"]
   s.require_paths = ["lib"]
   s.rubygems_version = "1.8.17"
-  s.summary = "TODO: one-line summary of your gem"
+  s.summary = "framework for writing spreadsheet file importers"
 
   if s.respond_to? :specification_version then
     s.specification_version = 3
 
     if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
+      s.add_runtime_dependency(%q<roo>, [">= 0"])
       s.add_development_dependency(%q<rdoc>, ["~> 3.12"])
       s.add_development_dependency(%q<bundler>, ["~> 1.0.0"])
       s.add_development_dependency(%q<jeweler>, ["~> 1.8.3"])
       s.add_development_dependency(%q<simplecov>, [">= 0"])
     else
+      s.add_dependency(%q<roo>, [">= 0"])
       s.add_dependency(%q<rdoc>, ["~> 3.12"])
       s.add_dependency(%q<bundler>, ["~> 1.0.0"])
       s.add_dependency(%q<jeweler>, ["~> 1.8.3"])
       s.add_dependency(%q<simplecov>, [">= 0"])
     end
   else
+    s.add_dependency(%q<roo>, [">= 0"])
     s.add_dependency(%q<rdoc>, ["~> 3.12"])
     s.add_dependency(%q<bundler>, ["~> 1.0.0"])
     s.add_dependency(%q<jeweler>, ["~> 1.8.3"])
--- lib/flatfile_importer/importer.rb
+++ lib/flatfile_importer/importer.rb
@@ -1,198 +1,194 @@
-class FlatfileImporter::Importer
-  attr_accessor :spreadsheet
-  attr_accessor :column_indices
+module FlatfileImporter
+  class Importer
+    attr_accessor :spreadsheet
+    attr_accessor :column_indices
   
-  def initialize(filepath)
-    read_excel(filepath)
-    detect_columns
-  end
-  
-  def import!
-    process_lines
-  end
-  
-  def read_excel(filepath)
-    begin
-      require 'iconv'
-      @spreadsheet = Excel.new(filepath)
-      @spreadsheet.default_sheet = @spreadsheet.sheets.first
-    rescue TypeError => err
-      raise err.message
+    def initialize(filepath)
+      read_excel(filepath)
+      detect_columns
     end
-  end
   
-  def detect_columns
-    attrs = [primary_key_attribute] +
-            primary_mass_assignable_attributes +
-            primary_complex_attributes
-    joins.each do |join|
-      attrs += keys_for(join)
-      attrs += attributes_for(join)
+    def import!
+      process_lines
     end
-    @column_indices = {}
-    attrs.each do |at|
-      ats = at.to_s
-      index = @spreadsheet.row(1).index {|item| item == ats}
-      if index
-        logger.info("#{at} is column #{index+1}")
-        @column_indices[at] = index+1
-      else
-        raise "Couldn't find a header cell labelled \"#{at}\". Nothing imported."
+  
+    def read_excel(filepath)
+      begin
+        require 'iconv'
+        @spreadsheet = Excel.new(filepath)
+        @spreadsheet.default_sheet = @spreadsheet.sheets.first
+      rescue TypeError => err
+        raise err.message
       end
     end
-  end
   
-  def primary_mass_assignable_attributes
-    raise "implement me"
-  end
-  
-  def primary_complex_attributes
-    raise "implement me"
-  end
-  
-  def joins
-    raise "implement me"
-  end
-  
-  def keys_for(join)
-    raise "implement me"
-  end
-  
-  def attributes_for(join)
-    raise "implement me"
-  end
-  
-  # How we key a primary record
-  def primary_key_attribute
-    raise "implement me"
-  end
-  
-  def assign_complex_attribute(record, attr_name, value)
-  end
-  
-  def primary_record_endpoint
-    raise "implement me"
-  end
-  
-  def cell_value(line, col_label)
-    @spreadsheet.cell(line, @column_indices[col_label]).to_s
-  end
-  
-  def process_lines
-    @primary_records = {}
-    @to_save = Set.new
-    import_time = DateTime.now
-        
-    (2..@spreadsheet.last_row).each do |line|
-      ms = time_block_ms {
-        process_line(line)
-      }
-      logger.info("Processing line #{line} took #{ms} ms")
-      @organisation.inc(:import_progress, 1)
-    end
-    
-    results = {}
-    @to_save.each do |record|
-      clazz = record.class
-      results[clazz] ||= {}
-      results[clazz][:updated] ||= []
-      results[clazz][:created] ||= []
-      results[clazz][:invalid] ||= []
-      existing = record.persisted?
-      saved = false
-      ms = time_block_ms {
-        record.last_imported_at = import_time if record.respond_to?(:last_imported_at)
-        saved = record.save
-      }
-      logger.info("Saving #{'existing ' if existing}#{record.class.name} #{record.id} took #{ms} ms")
-      @organisation.inc(:import_progress, 1)
-      if saved
-        if existing
-          results[clazz][:updated] << record
-        else
-          results[clazz][:created] << record
-        end
-      else
-        results[clazz][:invalid] << record
-      end
-    end
-    
-    results
-  end
-  
-  def self.import_excel(organisation, file)
-    pi = ProductImporter.new(file)
-    pi.import_to(organisation)
-  end
-  
-protected
-  
-  def logger
-    Rails.logger
-  end
-  
-private
-
-   
-  def process_line(line)
-    primary_key = cell_value(line, primary_key_attribute)
-    logger.info("processing line with primary key #{primary_key}")
-    
-    unless primary_key.blank?
-      primary_record = @primary_records[primary_key]
-      if !primary_record
-        logger.info("found primary record #{primary_record}")
-        primary_record = primary_record_endpoint.where(primary_key_attribute => primary_key).first
-        if primary_record
-          logger.info("found existing #{primary_key}")
-        else
-          logger.info("creating #{primary_key}")
-          primary_record = primary_record_endpoint.build(
-            primary_key_attribute => primary_key
-          )
-        end
-        
-        # Assign simple attributes
-        primary_mass_assignable_attributes.map do |attr_name|
-          primary_record.send("#{attr_name}=", cell_value(line, attr_name))
-        end
-        
-        # Handle attributes/relations with custom import behaviour
-        primary_complex_attributes.each do |attr_name|
-          assign_complex_attribute(primary_record, attr_name, cell_value(line, attr_name))
-        end
-      else
-        logger.info("already seen #{primary_key}")
-      end
-      
-      # Now deal with sub records
+    def detect_columns
+      attrs = [primary_key_attribute] +
+              primary_mass_assignable_attributes +
+              primary_complex_attributes
       joins.each do |join|
-        keys = Hash[*keys_for(join).map {|k| [k, cell_value(line, k)]}.flatten]
-        # Might have to load whole relation and do manual detect to get autosaving working here
-        secondary = primary_record.send(join).detect do |sec|
-          keys.all? do |k,v|
-            sec.send(k) == v
-          end
-        end
-        if secondary
-          logger.info("found secondary with keys #{keys.values.join(', ')}")
-        else
-          logger.info("creating new secondary for keys #{keys.values.join(', ')}")
-          secondary = primary_record.send(join).build(keys)
-        end
-        
-        # Assign simple attributes
-        attributes_for(join).map do |attr_name|
-          secondary.send("#{attr_name}=", cell_value(line, attr_name))
-        end
-        
-        @to_save << secondary unless primary_record.new_record?
+        attrs += keys_for(join)
+        attrs += attributes_for(join)
       end
-      
-      @primary_records[primary_key] = primary_record
-      @to_save << primary_record
-      
+      @column_indices = {}
+      attrs.each do |at|
+        ats = at.to_s
+        index = @spreadsheet.row(1).index {|item| item == ats}
+        if index
+          logger.info("#{at} is column #{index+1}")
+          @column_indices[at] = index+1
+        else
+          raise "Couldn't find a header cell labelled \"#{at}\". Nothing imported."
+        end
+      end
     end
-  end
   
+    def primary_mass_assignable_attributes
+      raise "implement me"
+    end
+  
+    def primary_complex_attributes
+      raise "implement me"
+    end
+  
+    def joins
+      raise "implement me"
+    end
+  
+    def keys_for(join)
+      raise "implement me"
+    end
+  
+    def attributes_for(join)
+      raise "implement me"
+    end
+  
+    # How we key a primary record
+    def primary_key_attribute
+      raise "implement me"
+    end
+  
+    def assign_complex_attribute(record, attr_name, value)
+    end
+  
+    def primary_record_endpoint
+      raise "implement me"
+    end
+  
+    def cell_value(line, col_label)
+      @spreadsheet.cell(line, @column_indices[col_label]).to_s
+    end
+  
+    def process_lines
+      @primary_records = {}
+      @to_save = Set.new
+      import_time = DateTime.now
+        
+      (2..@spreadsheet.last_row).each do |line|
+        ms = time_block_ms {
+          process_line(line)
+        }
+        logger.info("Processing line #{line} took #{ms} ms")
+        @organisation.inc(:import_progress, 1)
+      end
+    
+      results = {}
+      @to_save.each do |record|
+        clazz = record.class
+        results[clazz] ||= {}
+        results[clazz][:updated] ||= []
+        results[clazz][:created] ||= []
+        results[clazz][:invalid] ||= []
+        existing = record.persisted?
+        saved = false
+        ms = time_block_ms {
+          record.last_imported_at = import_time if record.respond_to?(:last_imported_at)
+          saved = record.save
+        }
+        logger.info("Saving #{'existing ' if existing}#{record.class.name} #{record.id} took #{ms} ms")
+        @organisation.inc(:import_progress, 1)
+        if saved
+          if existing
+            results[clazz][:updated] << record
+          else
+            results[clazz][:created] << record
+          end
+        else
+          results[clazz][:invalid] << record
+        end
+      end
+    
+      results
+    end
+  
+  protected
+  
+    def logger
+      Rails.logger
+    end
+  
+  private
+   
+    def process_line(line)
+      primary_key = cell_value(line, primary_key_attribute)
+      logger.info("processing line with primary key #{primary_key}")
+    
+      unless primary_key.blank?
+        primary_record = @primary_records[primary_key]
+        if !primary_record
+          logger.info("found primary record #{primary_record}")
+          primary_record = primary_record_endpoint.where(primary_key_attribute => primary_key).first
+          if primary_record
+            logger.info("found existing #{primary_key}")
+          else
+            logger.info("creating #{primary_key}")
+            primary_record = primary_record_endpoint.build(
+              primary_key_attribute => primary_key
+            )
+          end
+        
+          # Assign simple attributes
+          primary_mass_assignable_attributes.map do |attr_name|
+            primary_record.send("#{attr_name}=", cell_value(line, attr_name))
+          end
+        
+          # Handle attributes/relations with custom import behaviour
+          primary_complex_attributes.each do |attr_name|
+            assign_complex_attribute(primary_record, attr_name, cell_value(line, attr_name))
+          end
+        else
+          logger.info("already seen #{primary_key}")
+        end
+      
+        # Now deal with sub records
+        joins.each do |join|
+          keys = Hash[*keys_for(join).map {|k| [k, cell_value(line, k)]}.flatten]
+          # Might have to load whole relation and do manual detect to get autosaving working here
+          secondary = primary_record.send(join).detect do |sec|
+            keys.all? do |k,v|
+              sec.send(k) == v
+            end
+          end
+          if secondary
+            logger.info("found secondary with keys #{keys.values.join(', ')}")
+          else
+            logger.info("creating new secondary for keys #{keys.values.join(', ')}")
+            secondary = primary_record.send(join).build(keys)
+          end
+        
+          # Assign simple attributes
+          attributes_for(join).map do |attr_name|
+            secondary.send("#{attr_name}=", cell_value(line, attr_name))
+          end
+        
+          @to_save << secondary unless primary_record.new_record?
+        end
+      
+        @primary_records[primary_key] = primary_record
+        @to_save << primary_record
+      
+      end
+    end
+  
+  end
 end
